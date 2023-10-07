74738d7c0b9b1984f7207f8a997b00486a72a192
MatveyUtkov <103751684+MatveyUtkov@users.noreply.github.com>
2023-10-08 02:48:52 +0600

    1

--- main.py
+++ main.py
@@ -1,54 +1,39 @@
-import numpy as np
-import os
-from sklearn import svm
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split, GridSearchCV
 from skimage.io import imread
 from skimage.transform import resize
+import numpy as np
 
-# Load data
-DATA_DIR = "Agricultural-crops"
-CATEGORIES = os.listdir(DATA_DIR)
-IMG_SIZE = 64
-data = []
-labels = []
+# Load the dataset
+data_dir = 'Agricultural-crops'
+data = load_files(data_dir)
+images = [imread(file) for file in data.filenames]
+target = data.target
 
-for category in CATEGORIES:
-    path = os.path.join(DATA_DIR, category)
-    label = CATEGORIES.index(category)
+# Resize images and convert to grayscale (if they are in RGB)
+X = [resize(image, (64, 64)).mean(axis=2).ravel() for image in images]
 
-    for img_file in os.listdir(path):
-        img_path = os.path.join(path, img_file)
-        img = imread(img_path)
-        img_resized = resize(img, (IMG_SIZE, IMG_SIZE))
-        data.append(img_resized.flatten())
-        labels.append(label)
-
-data = np.array(data)
-labels = np.array(labels)
-
-# First split the data into 90% training+validation and 10% testing
-X_temp, X_test, y_temp, y_test = train_test_split(data, labels, test_size=0.1, random_state=42)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.1, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
 
 # Standardize the data
-scaler = StandardScaler()
-X_temp = scaler.fit_transform(X_temp)
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_val = scaler.transform(X_val)
 X_test = scaler.transform(X_test)
-kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-# SVM
-svm_accuracies = []
-for train_index, val_index in kf.split(X_temp):
-    X_train, X_val = X_temp[train_index], X_temp[val_index]
-    y_train, y_val = y_temp[train_index], y_temp[val_index]
+# Create the SVM model
+svm = SVC()
+svm.fit(X_train, y_train)
 
-    svm_clf = svm.SVC(kernel='linear', C=1)
-    svm_clf.fit(X_train, y_train)
+parameters = {'C': [0.1, 1, 10, 100]}
+clf = GridSearchCV(svm, parameters, cv=5)
+clf.fit(X_train, y_train)
 
-    y_pred = svm_clf.predict(X_val)
-    accuracy = accuracy_score(y_val, y_pred)
-    svm_accuracies.append(accuracy)
-avg_svm_accuracy = np.mean(svm_accuracies)
+print(f"Best parameters: {clf.best_params_}")
+accuracy = svm.score(X_test, y_test)
+print(f"Test accuracy: {accuracy:.2f}")
 
-print(f"Average SVM Accuracy: {avg_svm_accuracy * 100:.2f}%")
